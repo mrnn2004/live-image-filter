21665518ef7c7e82ce7f63c60812922af70ae17f
mohammad reza <1383mrnn@gmail.com>
2022-07-05 01:38:54 +0430

    run websocket client in localhost

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("wss://6d70-89-39-107-194.ngrok.io");
+var ws = new WebSocket("wss://localhost:3000");
 
 ws.onopen = function() {
     var img = document.getElementById('defaultCanvas0');
